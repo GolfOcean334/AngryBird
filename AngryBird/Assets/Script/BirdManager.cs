05ec95180c9e51215b2d2df01da53a058437d6a6
GolfOcean334 <130740013+GolfOcean334@users.noreply.github.com>
2025-03-18 22:51:49 +0100

    Add dash to the fast bird and random spawn bird

--- AngryBird/Assets/Script/BirdManager.cs
+++ AngryBird/Assets/Script/BirdManager.cs
@@ -1,13 +1,30 @@
+using System.Collections;
 using UnityEngine;
 
 public class BirdManager : MonoBehaviour
 {
-    [SerializeField] private GameObject birdPrefab;
+    [Header("Birds")]
+    [SerializeField] private GameObject normalBirdPrefab;
+    [SerializeField] private GameObject fastBirdPrefab;
     [SerializeField] private int maxBirds;
 
     private int remainingBirds;
     private Rigidbody2D bird;
     private Collider2D birdCollider;
+    private bool isFastBird;
+    private bool hasDashed;
+    private bool isLaunched;
+
+    [Header("Fast Bird Settings")]
+    [SerializeField] private float dashForce = 10f;
+
+    void Update()
+    {
+        if (isFastBird && bird != null && !hasDashed && Input.GetKeyDown(KeyCode.Space))
+        {
+            Dash();
+        }
+    }
 
     public void InitializeBirds()
     {
@@ -19,12 +36,17 @@ public class BirdManager : MonoBehaviour
     {
         if (remainingBirds > 0)
         {
+            GameObject birdPrefab = (Random.value > 0.5f) ? fastBirdPrefab : normalBirdPrefab;
             bird = Instantiate(birdPrefab).GetComponent<Rigidbody2D>();
             birdCollider = bird.GetComponent<Collider2D>();
             birdCollider.enabled = false;
 
             bird.isKinematic = true;
             remainingBirds--;
+
+            isFastBird = (birdPrefab == fastBirdPrefab);
+            hasDashed = false;
+            isLaunched = false;
         }
     }
 
@@ -43,7 +65,7 @@ public class BirdManager : MonoBehaviour
 
     public void UpdateBirdPosition(Vector3 position, Vector3 center, float offset)
     {
-        if (bird)
+        if (bird != null && !isLaunched)
         {
             Vector3 direction = position - center;
             bird.transform.position = position + direction.normalized * offset;
@@ -53,13 +75,22 @@ public class BirdManager : MonoBehaviour
 
     public void Shoot(Vector3 currentPosition, Vector3 centerPosition, float force)
     {
-        if (bird)
+        if (bird != null)
         {
             bird.isKinematic = false;
             Vector3 birdForce = (currentPosition - centerPosition) * force * -1;
             bird.velocity = birdForce;
-            bird = null;
-            birdCollider = null;
+            isLaunched = true;
+        }
+    }
+
+    private void Dash()
+    {
+        if (bird != null)
+        {
+            Vector2 dashDirection = bird.velocity.normalized;
+            bird.velocity += dashDirection * dashForce;
+            hasDashed = true;
         }
     }
 }
